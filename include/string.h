0342b041b237ad0b16c15c506d96fab701cc9056
Samuel Holland <samuel@sholland.org>
2017-10-16 02:19:12 -0500

    lib: Implement strlen
    Signed-off-by: Samuel Holland <samuel@sholland.org>

---
+++ include/string.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright © 2017 Samuel Holland <samuel@sholland.org>
+ * SPDX-License-Identifier: GPL-2.0
+ */
+
+#ifndef STRING_H
+#define STRING_H
+
+#include <types.h>
+
+size_t strlen(const char *s);
+
+#endif /* STRING_H */
