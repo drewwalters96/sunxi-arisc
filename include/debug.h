bbd282c781d99750a200e24729306abf34e6f755
Samuel Holland <samuel@sholland.org>
2017-10-16 02:32:12 -0500

    lib: Implement an assertion and logging framework
    Again, the only hardware-specific part is the implementation of the
    panic handler. For now it just spins.
    
    Signed-off-by: Samuel Holland <samuel@sholland.org>

---
+++ include/debug.h
@@ -0,0 +1,40 @@
+/*
+ * Copyright © 2017 Samuel Holland <samuel@sholland.org>
+ * SPDX-License-Identifier: GPL-2.0
+ */
+
+#ifndef DEBUG_H
+#define DEBUG_H
+
+#include <compiler.h>
+#include <stdarg.h>
+#include <types.h>
+
+#define LOG_LEVEL_FATAL   0
+#define LOG_LEVEL_ERROR   1
+#define LOG_LEVEL_WARNING 2
+#define LOG_LEVEL_INFO    3
+#define LOG_LEVEL_DEBUG   4
+
+#if DEBUG
+#define assert(e)         ((void)((e) || \
+	                          (panic("assertion failed: %s (%s:%d)", #e, \
+	                                 __FILE__, __LINE__), 0)))
+#else
+#define assert(e)         ((void)0)
+#endif
+
+#define panic(...)        (log(LOG_LEVEL_FATAL, __VA_ARGS__), do_panic())
+#define error(...)        log(LOG_LEVEL_ERROR, __VA_ARGS__)
+#define warn(...)         log(LOG_LEVEL_WARNING, __VA_ARGS__)
+#define info(...)         log(LOG_LEVEL_INFO, __VA_ARGS__)
+#if DEBUG
+#define debug(...)        log(LOG_LEVEL_DEBUG, __VA_ARGS__)
+#else /* !DEBUG */
+#define debug(...)
+#endif /* DEBUG */
+
+noreturn void do_panic(void);
+void log(unsigned level, const char *fmt, ...) __printf(2, 3);
+
+#endif /* DEBUG_H */
---
+++ lib/debug.c
@@ -0,0 +1,42 @@
+/*
+ * Copyright © 2017 Samuel Holland <samuel@sholland.org>
+ * SPDX-License-Identifier: GPL-2.0
+ */
+
+#include <compiler.h>
+#include <debug.h>
+#include <stdarg.h>
+#include <stdio.h>
+
+void
+log(unsigned level, const char *fmt, ...)
+{
+	const char *prefix;
+	va_list     args;
+
+	assert(level <= (DEBUG ? LOG_LEVEL_DEBUG : LOG_LEVEL_INFO));
+	switch (level) {
+	case LOG_LEVEL_FATAL:
+		prefix = "FATAL:\t";
+		break;
+	case LOG_LEVEL_ERROR:
+		prefix = "ERROR:\t";
+		break;
+	case LOG_LEVEL_WARNING:
+		prefix = "WARNING:";
+		break;
+	case LOG_LEVEL_INFO:
+		prefix = "INFO:\t";
+		break;
+	case LOG_LEVEL_DEBUG:
+		prefix = "DEBUG:\t";
+		break;
+	default:
+		unreachable();
+	}
+	puts(prefix);
+	va_start(args, fmt);
+	vprintf(fmt, args);
+	va_end(args);
+	putc('\n');
+}
