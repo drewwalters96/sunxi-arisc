957015db8c847522975ea7858dfc4a0fff5e48c3
Samuel Holland <samuel@sholland.org>
2017-10-18 23:15:22 -0500

    common: Add exception handling infrastructure
    Signed-off-by: Samuel Holland <samuel@sholland.org>

---
+++ common/exception.c
@@ -0,0 +1,23 @@
+/*
+ * Copyright © 2017 Samuel Holland <samuel@sholland.org>
+ * SPDX-License-Identifier: GPL-2.0
+ */
+
+#include <debug.h>
+#include <exception.h>
+#include <types.h>
+#include <arch/exception.h>
+
+void
+exception_handler(uint32_t number, struct exception_regs *regs)
+{
+	switch (number) {
+	case TICK_TIMER_EXCEPTION:
+		break;
+	case EXTERNAL_INTERRUPT:
+		break;
+	default:
+		panic("Unhandled exception %d at %p! (lr=%p)",
+		      number, (void *)regs->pc, (void *)regs->r9);
+	}
+}
